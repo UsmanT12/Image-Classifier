525692798b72d70b64a9b1ddd93844aa13d362e5
UsmanT12 <usmantahir21.ut@gmail.com>
2024-07-10 16:45:40 -0700

    Wrote some tests for functions from imageutils.py and genutils.py

--- genutils.py
+++ genutils.py
@@ -1,30 +1,24 @@
 #genutils.lib
 import sys
 
-    #In the genutils.lib, create a generic utility for parsing command-line params into a dict . It should take in lists of required and optional params. Optional params should be mapped to default values in the call. It should only succeed if the command-line satisfies the param requirements. On success, it should return a dict mapping each param to its assigned value. This will be a nice, reusable utility for all your Python programming.
+    #Generic utility for parsing command-line params into a dict . It should take in lists of required and optional params. Optional params should be mapped to default values in the call. It should only succeed if the command-line satisfies the param requirements. On success, it should return a dict mapping each param to its assigned value. This will be a nice, reusable utility for all your Python programming.
 def parse_command_line_params(required_params, optional_params):
-    #Check if the number of argurments is less than the number of required parameters
-    if len(sys.argv) - 1 < len(required_params):
-        print("Usage: python3 <filename> " + " ".join(required_params) + " " + " ".join(["[" + param + "]" for param in optional_params]))
-        sys.exit(1)
-        
-    #create a dictionary to store the parameters
-    params = {}
+    params_dict = {}
     args = sys.argv[1:]
     
-    # Parse required parameters
+    #Check if the number of arguments is correct
+    if len(args) < len(required_params):
+        raise ValueError("Not all required parameters are provided")
+    
+    # Assign values to required parameters
     for i, param in enumerate(required_params):
-        params[param] = args[i]
-    
-    #parse optional parameters
-    for param, default in optional_params.items():
+        params_dict[param] = args[i]
+
+    # Assign values to optional parameters
+    for param, default_value in optional_params.items():
         if param in args:
-            index = args.index(param)
-            if index + 1 < len(args):
-                params[param] = args[index + 1]
-            else:
-                params[param] = default
+            params_dict[param] = args[args.index(param) + 1]
         else:
-            params[param] = default
-    
-    return params 
+            params_dict[param] = default_value
+
+    return params_dict
--- lib_tests.py
+++ lib_tests.py
@@ -6,13 +6,28 @@ import os
 import sys
 
 #Tests for functions from imageutils.py
-image = Image.open('usman_pic copy.jpg')
+image = resize_image('usman_pic copy.jpg', 200, 200)
+image2 = resize_image('JPEG/good_cup.jpg', 200, 200)
+
 '''
 print (resize_image('usman_pic copy.jpg', 200, 200))
 print(np.array(image))
 print('************')
 print(column_vector(image))
 '''
+
+'''
+#Test for combined vectors and column vectors
+image_vector_1 = column_vector(image)
+image_vector_2 = column_vector(image2)
+combined = combine_vectors(image_vector_1, image_vector_2)
+print('image_vector_1: ', image_vector_1)
+print('***********')
+print('image_vector_2: ', image_vector_2)
+print('***********')
+print('combined vectors: ', combined)
+'''
+
 
 '''
 #Test for load_images
@@ -26,3 +41,18 @@ except Exception as e:
     print(f"Error loading images: {e}")
 '''
 
+
+
+#Tests for the genutils.py functions
+def main():
+    required_params = ['input_file', 'output_file']
+    optional_params = {'--verbose': 'False', '--mode': 'default'}
+    
+    try:
+        params = parse_command_line_params(required_params, optional_params)
+        print("Parsed parameters:", params)
+    except ValueError as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    main()
